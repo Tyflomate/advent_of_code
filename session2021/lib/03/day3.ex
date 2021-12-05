fcb18b0d5b6b3e30f726fdba048947d4d828dc28
Tyflomate <florian@tymate.com>
2021-12-05 22:28:49 +0100

    Add day 3 part2

--- session2021/lib/03/day3.ex
+++ session2021/lib/03/day3.ex
@@ -26,13 +26,35 @@ defmodule Day3 do
     {{min, _}, {max, _}} =
       binary_list
       |> Enum.frequencies()
-      |> Enum.min_max_by(fn {_, value} -> value end)
+      |> Enum.min_max_by(fn {x, value} -> {value, x} end)
 
     [min, max]
   end
 
+  def rate_from_input([], _, _) do [] end
+  def rate_from_input([x | []], _, _), do: x
+  def rate_from_input(input, index, oxygen_rate?) do
+    [min, max] = input |> List.zip |> Enum.at(index) |> Tuple.to_list |> Day3.min_max_from
+    checked_value = if oxygen_rate?, do: max, else: min
+
+    input
+    |> Enum.filter(fn xs -> Enum.at(xs, index) == checked_value end)
+    |> rate_from_input(index + 1, oxygen_rate?)
+  end
+
+  def oxygen_rate(input, index \\ 0)
+  def oxygen_rate(input, index) do
+    rate_from_input(input, index, true) |> List.to_string |> String.to_integer(2)
+  end
+
+  def co2_rate(input, index \\ 0)
+  def co2_rate(input, index) do
+    rate_from_input(input, index, false) |> List.to_string |> String.to_integer(2)
+  end
+
   def part2 do
-    input()
-    #|>
+    oxygen_rate = input() |> oxygen_rate
+    co2_rate = input() |> co2_rate
+    oxygen_rate * co2_rate
   end
 end
